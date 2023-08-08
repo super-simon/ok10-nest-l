acac9078058bc030c9fcc476e259c7a98bd6e132
Oleksandr Stetsiuk <oleksandr.v.stetsiuk@gmail.com>
2023-08-08 20:41:51 +0300

    postgres configuration

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,11 +1,17 @@
 import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
 import { AnimalModule } from './animal/animal.module';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { TypeOrmConfiguration } from './config/database/type-orm-configuration';
 import { UserModule } from './user/user.module';
 
 @Module({
-  imports: [UserModule, AnimalModule],
+  imports: [
+    UserModule,
+    AnimalModule,
+    TypeOrmModule.forRootAsync(TypeOrmConfiguration.config),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
--- src/main.ts
+++ src/main.ts
@@ -1,7 +1,12 @@
 import { ValidationPipe } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
 import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
+import * as dotenv from 'dotenv';
 import { AppModule } from './app.module';
+
+const environment = process.env.NODE_ENV ?? '';
+
+dotenv.config({ path: `environments/${environment}.env` });
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
